8f93e73170e887dc01f557ad471fd8a3d99a1bcc
Lukas Ewecker <lukas.ewecker@porsche.de>
2021-01-28 10:57:43 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+import os
+
+
+lib_dir = os.path.dirname(os.path.realpath(__file__))
+requirements_path = lib_dir + "/requirements.txt"
+install_requires = []
+if os.path.isfile(requirements_path):
+    with open(requirements_path) as f:
+        install_requires = f.read().splitlines()
+
+setup(
+    name='pvdn',
+    version='0.1',
+    packages=['pvdn', 'pvdn.filters'],
+    url='https://github.com/larsOhne/pvdn',
+    license='Creative Commons Legal Code ',
+    author='Lars Ohnemus, Lukas Ewecker, Ebubekir Asan, Sascha Saralajew',
+    author_email='',
+    description='Tools for working with the PVDN dataset.',
+    install_requires=install_requires
+)
