d8c42911ecd8161c57d23bd9a58d88ecca27d73b
Lukas Ewecker <lukasewecker@web.de>
2022-03-14 19:15:06 +0100

    [feat] finish script to evaluate groundtruth quality for all splits

--- pvdn/detection/scripts/evaluate_groundtruth_quality.py
+++ pvdn/detection/scripts/evaluate_groundtruth_quality.py
@@ -1,7 +1,9 @@
 from pvdn.metrics.convert import gt_to_results_format
 from pvdn.metrics import BoundingBoxEvaluator
+from pvdn import BoundingBoxDataset
 
 import argparse
+import os
 
 
 if __name__ == "__main__":
@@ -10,3 +12,13 @@ if __name__ == "__main__":
     )
     parser.add_argument("--data_dir", type=str, help="Path to the data split to be "
                                                      "used.", required=True)
+    opts = parser.parse_args()
+
+    for split in ("train", "test", "val"):
+        ds = BoundingBoxDataset(path=os.path.join(opts.data_dir, split),
+                                blob_detector=None)
+        results = gt_to_results_format(ds.bounding_box_path)
+        evaluator = BoundingBoxEvaluator(os.path.join(opts.data_dir, split))
+        evaluator.load_results_from_dict(results)
+        performance = evaluator.evaluate(verbose=True)
+        print(f"{split}:", performance)
